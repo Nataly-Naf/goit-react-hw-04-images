c9ea8d9140dd0bf53997932951a46608892ffe4e
Nataly-Naf <nat.spivak84@gmail.com>
2023-10-17 18:57:11 +0100

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import { GetFilteredImage } from './Api/Api';
 import { ImageGallery } from './ImageGallery/ImageGallery';
 import { Searchbar } from './Searchbar/Searchbar';
@@ -7,68 +7,90 @@ import toast, { Toaster } from 'react-hot-toast';
 import { AudioItem } from './Loader/Loader';
 import { AppS } from './App.styled';
 
-export class App extends Component {
-  state = {
-    pictures: [],
-    query: 'dog',
-    loading: false,
-    error: false,
-    page: 1,
+export const App = () => {
+  const [pictures, setPictures] = useState([]);
+  const [query, setQuery] = useState('');
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+  const [page, setPage] = useState(1);
+
+  const onSubmitForm = filter => {
+    setQuery(filter);
   };
 
-  onSubmitForm = filter => {
-    console.log(filter);
-    this.setState({ page: 1, pictures: [] });
-    return this.setState({ query: filter });
-  };
-
-  loadMoreHandler = () => {
-    this.setState(prevState => {
-      return { page: prevState.page + 1 };
-    });
-  };
-
-  async componentDidUpdate(prevProps, prevState) {
-    if (
-      prevState.query !== this.state.query ||
-      prevState.page !== this.state.page
-    ) {
+  useEffect(() => {
+    if (query === '') {
+      return;
+    }
+    async function getPicture() {
       try {
-        this.setState({ loading: true, error: false });
-
-        const newPictures = await GetFilteredImage(
-          this.state.query,
-          this.state.page
-        );
+        setError(false);
+        setLoading(true);
+        const newPictures = await GetFilteredImage(query, page);
         console.log(newPictures);
         toast.success('We have found pictures');
-        this.setState(prevState => {
-          return {
-            pictures: [...prevState.pictures, ...newPictures],
-          };
-        });
+        setPictures(prevState => [...prevState, ...newPictures]);
       } catch (error) {
-        this.setState({ error: true });
+        setError(true);
       } finally {
-        this.setState({ loading: false });
+        setLoading(false);
       }
     }
-  }
-  render() {
-    const { loading, error } = this.state;
-    return (
-      <AppS>
-        <Searchbar onSubmitForm={this.onSubmitForm} />
-        {loading && <AudioItem />}
+    getPicture();
+    console.log(pictures);
+  }, [query, page]);
 
-        <ImageGallery onPictures={this.state.pictures} />
-        {loading && <b>Loading pictures...</b>}
-        {error && <b>Whoops! Error! Please reload this page!!!</b>}
-        {this.state.pictures.length > 0 && (
-          <Button onLoadMoreBtn={this.loadMoreHandler} />
-        )}
-        <Toaster position="top-right" />
-      </AppS>
-    );
-  }
-}
+  const loadMoreHandler = () => {
+    setPage(prevState => prevState + 1);
+  };
+
+  return (
+    <AppS>
+      <Searchbar onSubmitForm={onSubmitForm} />
+      {loading && <AudioItem />}
+      <ImageGallery onPictures={pictures} />
+      {loading && <b>Loading pictures...</b>}
+      {error && <b>Whoops! Error! Please reload this page!!!</b>}
+      {pictures.length > 0 && <Button onLoadMoreBtn={loadMoreHandler} />}
+      <Toaster position="top-right" />
+    </AppS>
+  );
+};
+
+// loadMoreHandler = () => {
+//   this.setState(prevState => {
+//     return { page: prevState.page + 1 };
+//   });
+// };
+
+//   async componentDidUpdate(prevProps, prevState) {
+//     if (
+//       prevState.query !== this.state.query ||
+//       prevState.page !== this.state.page
+//     ) {
+//       try {
+//         this.setState({ loading: true, error: false });
+
+//         const newPictures = await GetFilteredImage(
+//           this.state.query,
+//           this.state.page
+//         );
+//         console.log(newPictures);
+//         toast.success('We have found pictures');
+//         this.setState(prevState => {
+//           return {
+//             pictures: [...prevState.pictures, ...newPictures],
+//           };
+//         });
+//       } catch (error) {
+//         this.setState({ error: true });
+//       } finally {
+//         this.setState({ loading: false });
+//       }
+//     }
+//   }
+//   render() {
+//     const { loading, error } = this.state;
+
+//   }
+// }
